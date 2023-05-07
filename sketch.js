5982615c664bf8b4b42abf22d2b7b69812e7a309
milosh3411 <milos.radenkovic@microcloud.rs>
2023-05-07 11:51:41 +0200

    radius

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
-let period;
+let period1,period2,period3;
 let fps;
-let period_marks;
-let v1_marks;
+let period_marks1,period_marks2,period_marks3;
+let v1_marks,v2_marks,v3_marks;
+let v1min,v1max,v2min,v2max,v3min,v3max;
 function setup() {
   let w = 540;
   let h = 960;
@@ -11,38 +12,50 @@ function setup() {
   stroke(0, 0, 0, 130);
   strokeWeight(8);
   //set frame per second to 60
-  fps = 24;
+  fps = 54;
   frameRate(fps);
-  period = 12;
-  period_marks = [0, 20, 30, 80, 100];
-  v1_marks =     [0, 20, 50, 100, 100];
+  period1 = 12;
+  period2 = 6;
+  period_marks1 = [0, 20, 30, 80, 100];
+  period_marks2 = [0, 100];
+  v2_marks = [0, 100];
+  v1_marks = [0, 20, 50, 100, 100];
+  v1min = 0;
+  v1max = 400;
+  v2min = 90;
+  v2max = 900;
 }
 function draw() {
   // let a be a value between 0 and 2PI proportional to the frameCount modulo 360
   let a = map(frameCount % 360, 0, 360, 0, TWO_PI);
-  let pp = get_period_percent(period, fps, "tooth-saw");
-  let v1 =
-    400 -
-    get_value(
-      get_period_percent(period, fps, "tooth-saw"),
-      period_marks,
-      400,
-      v1_marks
-    );
-  console.log("period percent: " + floor(pp), "v1: " + floor(v1));
+  let pp1 = get_period_percent(period1, fps);
+  let pp2 = get_period_percent(period2, fps);
+  let v2 = get_value(pp2, period_marks2, v2min, v2max, v2_marks);
+  let v1 = 400 - get_value(pp1, period_marks1, v1min, v1max, v1_marks);
+  //console.log("period percent: " + floor(pp), "value: " + v,"v1: " + floor(v1));
   //background(232, 220, 184); //aged paper color
   //background(225,241,253); //light blue
   background(253, 184, 19); //orange
   //rotatedConcentricCircles(270, 480, 10, 0, 40, 7, 3*a);
   stroke(0, 0, 0, 130);
-  push()
-  translate(270, 480)
-  rotate(a/2)
-  rotatedConcentricCircles(0, 0, 10, 90, 900, 21, 3*a);
-  rotatedConcentricCircles(0, 0 - v1, 20, 90, 959, 24.1, 2*a);
-  rotatedConcentricCircles(0, 0 + v1, -20, 91, 961, 26.5, -2*a);
-  pop()
+  push();
+  translate(270, 480);
+  rotate(a / 2);
+  rotatedConcentricCircles(0, 0, 10, 80, v2, 21, 3 * a);
+  rotatedConcentricCircles(0, 0 - v1, 20, 80, v2, 24.1, 2 * a);
+  rotatedConcentricCircles(0, 0 + v1, -20, 81, v2, 26.5, -2 * a);
+  pop();
   //stroke(0,155,214,130);
   //rotatedConcentricCircles(270, 480, 10, 400, 430, 19, -a);
-  //text(sec + "s", 10, 10)   
+  //text(sec + "s", 10, 10)
+}
+// toggle noloop on enter
+function keyPressed() {
+  if (keyCode === ENTER) {
+    if (isLooping()) {
+      noLoop();
+    } else {
+      loop();
+    }
+  }
 }
--- toolbox.js
+++ toolbox.js
@@ -1,54 +1,45 @@
-function get_period_percent(period, fps, type) {
-  let fc = frameCount % (fps * period);
-  if (type == "jig-saw") {
-    return (fc * 100) / (fps * period);
-  } else if (type == "tooth-saw") {
-    let fc_ts;
-    if (isEvenDivision(frameCount, fps * period)) {
-      fc_ts = fc;
-    } else {
-      fc_ts = fps * period - fc - 1;
-    }
-    return (fc_ts * 100) / (fps * period);
+function get_period_percent(period, fps) {
+  let cycle_number = floor(frameCount / (fps * period));
+  let counter = frameCount % (fps * period);
+  let evenCycle = cycle_number % 2;
+  if (evenCycle == 0) {
+    return (counter * 100) / (fps * period);
+  } else if (evenCycle == 1) {
+    return ((fps * period - counter) * 100) / (fps * period);
   }
 }
-function get_value(period_percent, period_marks, v_max, v_mark) {
+function get_value(period_percent, period_marks, v_min, v_max, v_mark) {
   let i = 0;
   let j = 0;
   //find current time range
   for (let k = 0; k < period_marks.length - 1; k++) {
-    if (
-      period_percent >= period_marks[k] &&
-      period_percent < period_marks[k + 1]
-    ) {
-      i = k;
-      j = k + 1;
+    if (period_percent == 100) {
+      i = period_marks.length - 2;
+      j = period_marks.length - 1;
+    } else {
+      if (
+        period_percent >= period_marks[k] &&
+        period_percent < period_marks[k + 1]
+      ) {
+        i = k;
+        j = k + 1;
+      }
     }
   }
   let v;
   if (v_mark[i] == v_mark[j]) {
-    v = (v_max * v_mark[i]) / 100;
+    v = v_min + ((v_max - v_min) * v_mark[i]) / 100;
   } else {
     v =
-      v_max *
-      map(
-        period_percent,
-        period_marks[i],
-        period_marks[j],
-        v_mark[i] / 100,
-        v_mark[j] / 100
-      );
+      v_min +
+      (v_max - v_min) *
+        map(
+          period_percent,
+          period_marks[i],
+          period_marks[j],
+          v_mark[i] / 100,
+          v_mark[j] / 100
+        );
   }
   return v;
-}
-//function that divides two whole numbers, calculates the whole number part of result and the remainder, and
-// returns true if the whole number part is even, and false if the whole number part is odd
-function isEvenDivision(numerator, denominator) {
-  let whole = floor(numerator / denominator);
-  let remainder = numerator % denominator;
-  if (whole % 2 == 0) {
-    return true;
-  } else {
-    return false;
-  }
 }
